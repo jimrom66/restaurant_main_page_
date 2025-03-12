8d5d86187b50b12e268f605fb6fcfd1a1c5afe0a
jimrom66 <188324624+jimrom66@users.noreply.github.com>
2025-03-12 13:18:15 +0200

    initial commit new

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import {about} from "./about.js";
+import {home} from "./home.js";
+import {menu} from "./menu.js";
+
+
+console.log(about);
+console.log(home);
+console.log(menu);
